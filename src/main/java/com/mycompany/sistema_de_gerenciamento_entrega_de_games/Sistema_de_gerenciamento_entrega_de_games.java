fe5438f960c6d8e9b53952a164acb1a352179794
ARTHUR ANTONIO DE ARAUJO <arthur.7923@aluno.pr.senac.br>
2023-11-01 15:39:16 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/sistema_de_gerenciamento_entrega_de_games/Sistema_de_gerenciamento_entrega_de_games.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema_de_gerenciamento_entrega_de_games;
+
+/**
+ *
+ * @author arthur.7923
+ */
+public class Sistema_de_gerenciamento_entrega_de_games {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
